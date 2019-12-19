be72225ca95db25be8ae7befaa113f5c18aceaea
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2019-12-19 07:48:30 +0545

    Initial commit and design

---
+++ app/src/main/java/com/trilochan/databaset_7/Helper/MyHelper.java
@@ -0,0 +1,26 @@
+package com.trilochan.databaset_7.Helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyHelper extends SQLiteOpenHelper {
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public MyHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+}
