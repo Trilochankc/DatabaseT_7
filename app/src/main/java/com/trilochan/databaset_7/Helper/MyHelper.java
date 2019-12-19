b728baad6562f9672d5c2e245d0d2411754ff56e
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2019-12-19 07:56:31 +0545

    DATABASE CREATE

--- app/src/main/java/com/trilochan/databaset_7/Helper/MyHelper.java
+++ app/src/main/java/com/trilochan/databaset_7/Helper/MyHelper.java
@@ -8,10 +8,25 @@ import androidx.annotation.Nullable;
 
 public class MyHelper extends SQLiteOpenHelper {
 
+private static  final String databaseName = "DictionaryDb";
+private static final int dbversion = 1;
+//tblwords fields
+
+    private static final String tblWord = "tblWord";
+    private static  final String WordID = "WordId";
+    private static final String Word = "Word";
+    private static final String Meaning = "Meaning";
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        
+
+        String query = "CREATE TABLE " + tblWord +
+                "("
+                + WordID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                Word + " TEXT,"
+                + Meaning + " TEXT" +
+        ")";
+        db.execSQL(query);
     }
 
     @Override
